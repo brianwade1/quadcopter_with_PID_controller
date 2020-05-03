da2121071b54a23442c5ba9925a3539c755b1d9e
brian.wade <brian.wade@nps.edu>
2020-05-03 14:54:40 -0700

    Added license info to main

--- Quadcopter_main.py
+++ Quadcopter_main.py
@@ -1,7 +1,24 @@
 '''
 Quadcopter simulation
-Written by: Brian Wade, 2 Feb 20
-Developed from original MATLAB version into python
+Written by: Brian Wade
+Date: 2 Feb 2020
+
+
+%Copyright (C) 2020  Brian M. Wade
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 References:
 1. Johnson, Nicholas Andrew; Control of a Folding Quadcopter with A Slung
